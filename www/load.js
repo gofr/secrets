4623d5cb254ae9e36150cc5d589979c010d28946
gofr <32750931+gofr@users.noreply.github.com>
2020-11-07 19:55:20 +0100

    Move JS loading logic to separate file
    Some more changes:
    * Stop redirecting to other hosts.
    * Strip the hash from the URL if it's the encryption key.

---
+++ www/load.js
@@ -0,0 +1,41 @@
+(function() {
+
+function isValidKey(key) {
+    try {
+        return atob(key).length == 16;
+    } catch (e) {
+        return false;
+    }
+}
+
+function getLastPathComponent(path) {
+    let components = path.split('/');
+    components.reverse();
+    return components.find(item => item);
+}
+
+let anchor = location.hash.substr(1);
+if (anchor && isValidKey(anchor)) {
+    sessionStorage.setItem('key', anchor);
+    history.replaceState({}, '', location.href.replace(location.hash, ''));
+}
+sessionStorage.setItem('dir', getLastPathComponent(location.pathname));
+
+let ogTitle = document.head.querySelector('meta[property="og:title"]');
+if (ogTitle) {
+    let title = ogTitle.getAttribute('content');
+    if (title) {
+        titleElement = document.createElement('title');
+        titleElement.textContent = title;
+        document.head.appendChild(titleElement);
+    }
+}
+addEventListener('DOMContentLoaded', () => {
+    decryptContent(sessionStorage.getItem('key'), 'content')
+        .then(value => {
+            document.body.appendChild(value);
+        })
+        .catch(reason => console.log(reason));
+});
+
+})();
