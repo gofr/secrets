205dc043634f1ce9877bbbb2340cb3de48f7e4db
gofr <32750931+gofr@users.noreply.github.com>
2020-12-26 19:31:16 +0100

    Expand interface of blog Post class
    Make it possible to create posts programmatically instead of only
    by loading text files.

--- encrypt.py
+++ encrypt.py
@@ -82,7 +82,7 @@ class Blog:
         posts = {}
         for filename in os.listdir(input_dir):
             if filename.endswith('.md'):
-                posts[filename] = Post(os.path.join(input_dir, filename))
+                posts[filename] = Post.from_file(os.path.join(input_dir, filename))
         self.posts = posts
 
     def load_config(self):
@@ -141,10 +141,11 @@ class Blog:
             # templates changed, especially with my output being encrypted.
             # Secondly, since image files are stored with new random names, this
             # will leave behind now unused files from previous rounds.
-            post.write(os.path.join(output_dir, post_dir), template_dir, key)
+            post.publish(os.path.join(output_dir, post_dir), template_dir, key)
 
 
 # TODO: unittest this class.
+# TODO: add methods to manipulate post sections.
 class Post:
     _metadata_pattern = re.compile(
         r'^(?:(?P<key>[a-z0-9_]+): (?P<value>.+)|# .*||(?P<error>.*))$',
@@ -152,9 +153,29 @@ class Post:
 
     # TODO: Require a post to be associated with a Blog? In that case I can set
     # global options on the blog that get used by posts.
-    def __init__(self, file_path):
+    def __init__(self, content=None, title=None):
+        self.location = None
+        self.title = title
+        self._sections = []
+        if content:
+            self.load(content)
+
+    @classmethod
+    def from_file(cls, file_path):
+        """Return a new Post from the content in the given `file_path`."""
         with open(file_path, 'rt') as f:
-            sections = re.split(r'\n\n---\n', f.read(), flags=re.MULTILINE)
+            text = f.read()
+        post = cls(text)
+        post.location = os.path.dirname(file_path)
+        return post
+
+    def load(self, content, override_title=False):
+        """Parse string `content` and load it into the Post object.
+
+        If `override_title` is `True` and the metadata in the `content` string
+        contains a post title, override the existing title.
+        """
+        sections = re.split(r'\n\n---\n', content, flags=re.MULTILINE)
 
         metadata = {}
         parsed_sections = []
@@ -168,21 +189,17 @@ class Post:
             elif not section.strip():
                 continue
             else:
-                parsed_sections.append(self.parse_commonmark(section))
-        self.location = os.path.dirname(file_path)
-        self.sections = parsed_sections
-        self.title = metadata.get('title')
+                parsed_sections.append(section)
+        self._sections = parsed_sections
+        title = metadata.get('title')
+        if title is not None and override_title:
+            self.title = title
 
-    def parse_commonmark(self, content):
-        """Parse CommonMark and return rendered HTML."""
-        parser = commonmark.Parser()
-        renderer = HTMLRenderer()
-        return renderer.render(parser.parse(content))
-
-    def parse_metadata(self, content):
+    @classmethod
+    def parse_metadata(cls, content):
         """Parse key-value string and return dict."""
         data = {}
-        for match in re.finditer(self._metadata_pattern, content):
+        for match in re.finditer(cls._metadata_pattern, content):
             key = match.group('key')
             error = match.group('error')
             if key is not None:
@@ -194,8 +211,33 @@ class Post:
                 raise ValueError(f'Invalid key-value data in line:\n{match.group(0)}')
         return data
 
+    @classmethod
+    def dump_metadata(cls, metadata):
+        """Return metadata dumped to string in expected content input format."""
+        # TODO: Validate metadata?
+        return '\n'.join([f'{key}: {value}' for key, value in metadata.items()])
+
+    @classmethod
+    def render_commonmark(cls, content):
+        """Parse CommonMark and return rendered HTML."""
+        parser = commonmark.Parser()
+        renderer = HTMLRenderer()
+        return renderer.render(parser.parse(content))
+
+    def dump(self):
+        """Dump the post back to a mixed CommonMark string."""
+        dumped_sections = []
+        if self.title:
+            dumped_sections.append(f'---\ntitle: {self.title}')
+        for section in self._sections:
+            if isinstance(section, dict):
+                dumped_sections.append(self.dump_metadata(section))
+            else:
+                dumped_sections.append(section.strip())
+        return '\n\n---\n'.join(dumped_sections)
+
     # TODO: Move encryption elsewhere?
-    def write(self, output_dir, template_dir, key):
+    def publish(self, output_dir, template_dir, key):
         """Write blog post to encrypted files in `output_dir`.
 
         Render the content using templates from `template_dir`.
@@ -214,7 +256,7 @@ class Post:
         os.makedirs(output_dir, exist_ok=True)
         encrypted_images = {}
         has_panorama = False
-        copied_sections = copy.deepcopy(self.sections)
+        copied_sections = copy.deepcopy(self._sections)
         for section in copied_sections:
             if isinstance(section, dict) and 'image' in section:
                 abs_path = os.path.abspath(os.path.join(self.location, section['image']))
@@ -228,6 +270,8 @@ class Post:
                     has_panorama = has_panorama or is_panorama
                 else:
                     section = encrypted_images[abs_path]
+            elif isinstance(section, str):
+                section = self.render_commonmark(section)
         content_template = env.get_template('content.html')
         content = content_template.render(content=copied_sections)
         encrypt(content.encode(), key, os.path.join(output_dir, 'content'))
