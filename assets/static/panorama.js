e60c9e9f864297a66a40f75fb7fd9432097e50de
gofr <32750931+gofr@users.noreply.github.com>
2021-01-05 17:24:30 +0100

    Zoom in to panorama if range is smaller than view
    If the vertical range on a cropped panorama photo is smaller than
    the default viewer zoom level, zoom in more. Avoid showing black
    areas.
    
    This obsoletes my change to minFov. That was another attempt at
    solving the same problem. But the only thing that did was change
    the default zoom level. That's halfway between minFov and maxFov.

--- assets/static/panorama.js
+++ assets/static/panorama.js
@@ -20,6 +20,19 @@
             return [longitude(p.croppedX), longitude(p.croppedX + p.croppedWidth)];
         }
     }
+    function fillView(viewer) {
+        // If the vertical range of the panorama does not fill the view,
+        // zoom in to fill the height (to at most minFov).
+        const visibleRangePlugin = viewer.getPlugin(PhotoSphereViewer.VisibleRangePlugin);
+        const latitude = visibleRangePlugin.config.latitudeRange;
+        if (latitude !== null) {
+            const view = 180 * Math.abs(latitude[1] - latitude[0]) / Math.PI;
+            if (view < viewer.prop.vFov) {
+                viewer.zoom(viewer.dataHelper.fovToZoomLevel(view));
+            }
+        }
+
+    }
     async function panoramaCallback(entries, observer) {
         for (let entry of entries) {
             if (entry.isIntersecting && 'panorama' in entry.target.dataset) {
@@ -32,7 +45,6 @@
                     "panorama": blob,
                     "navbar": false,
                     "mousewheel": false,
-                    "minFov": 20,
                     "touchmoveTwoFingers": true,
                     "plugins": [
                         PhotoSphereViewer.VisibleRangePlugin,
@@ -44,6 +56,7 @@
                     // TODO: Drop this and use upstreamed fix (e5da3944).
                     visibleRangePlugin.setLongitudeRange(getLongitudeRange(viewer.prop.panoData));
                     visibleRangePlugin.setLatitudeRange(getLatitudeRange(viewer.prop.panoData));
+                    fillView(viewer);
                 });
                 break;
             }
