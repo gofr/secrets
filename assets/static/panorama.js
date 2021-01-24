e4d056ee32a188a7d04f6dd18b38c10000acbd4b
gofr <32750931+gofr@users.noreply.github.com>
2021-01-24 16:19:27 +0100

    Update Photo Sphere Viewer to 4.1.0
    This version includes the "usePanoData" option for the
    VisibleRangePlugin, so drop my own fix.

--- assets/static/panorama.js
+++ assets/static/panorama.js
@@ -2,24 +2,6 @@
 (function() {
     var viewer;
 
-    function getLatitudeRange(panoData) {
-        const p = panoData;
-        if (p.croppedHeight == p.fullHeight && p.croppedY == 0) {
-            return null;
-        } else {
-            const latitude = y => Math.PI * (1 - y / p.fullHeight) - (Math.PI / 2);
-            return [latitude(p.croppedY), latitude(p.croppedY + p.croppedHeight)];
-        }
-    }
-    function getLongitudeRange(panoData) {
-        const p = panoData;
-        if (p.croppedWidth == p.fullWidth && p.croppedX == 0) {
-            return null;
-        } else {
-            const longitude = x => 2 * Math.PI * (x / p.fullWidth) - Math.PI;
-            return [longitude(p.croppedX), longitude(p.croppedX + p.croppedWidth)];
-        }
-    }
     function fillView(viewer) {
         // If the vertical range of the panorama does not fill the view,
         // zoom in to fill the height (to at most minFov).
@@ -47,15 +29,12 @@
                     "mousewheel": false,
                     "touchmoveTwoFingers": true,
                     "plugins": [
-                        PhotoSphereViewer.VisibleRangePlugin,
+                        [PhotoSphereViewer.VisibleRangePlugin, {usePanoData: true}]
                     ],
                 });
                 viewer.on('ready', function() {
                     URL.revokeObjectURL(blob);
                     const visibleRangePlugin = viewer.getPlugin(PhotoSphereViewer.VisibleRangePlugin);
-                    // TODO: Drop this and use upstreamed fix (e5da3944).
-                    visibleRangePlugin.setLongitudeRange(getLongitudeRange(viewer.prop.panoData));
-                    visibleRangePlugin.setLatitudeRange(getLatitudeRange(viewer.prop.panoData));
                     fillView(viewer);
                 });
                 break;
